096bddfe4d5d0371beaa967f779d0afa77ebdd71
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-05 22:22:02 +0200

    Added User init.

---
+++ src/main/java/com/marcinwo/todolist/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.marcinwo.todolist.controller;
+
+import com.marcinwo.todolist.repository.UserRepository;
+import com.marcinwo.todolist.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+
+
+}
