b656a864339d734b919a930f2b846f0360df95bc
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-05 21:55:49 +0200

    Added Role init.

---
+++ src/main/java/com/marcinwo/todolist/entity/Role.java
@@ -0,0 +1,21 @@
+package com.marcinwo.todolist.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role extends AbstractEntity {
+
+    private String name;
+
+}
