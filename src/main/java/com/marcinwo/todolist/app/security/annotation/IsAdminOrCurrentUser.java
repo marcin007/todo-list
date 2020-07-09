2e2bdc046ec73deb2cfb5191dc4cb52c3b0539ee
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-09 18:59:41 +0200

    Added new annotations init.

---
+++ src/main/java/com/marcinwo/todolist/app/security/annotation/IsAdminOrCurrentUser.java
@@ -0,0 +1,14 @@
+package com.marcinwo.todolist.app.security.annotation;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('ROLE_ADMIN') or #username == authentication.principal.username")
+public @interface IsAdminOrCurrentUser {
+}
