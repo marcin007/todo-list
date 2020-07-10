84b0d296f03985a28113db3105e7850c1ec76754
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-10 17:44:06 +0200

    Added UserDTO init.

---
+++ src/main/java/com/marcinwo/todolist/api/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package com.marcinwo.todolist.api.dto;
+
+public class UserDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+}
