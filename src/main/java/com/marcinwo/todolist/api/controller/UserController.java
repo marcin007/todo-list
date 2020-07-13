18217f1eac6bd4e52d5ad151c522f046d69c8314
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-13 18:14:45 +0200

    Added UserController init.

---
+++ src/main/java/com/marcinwo/todolist/api/controller/UserController.java
@@ -0,0 +1,78 @@
+package com.marcinwo.todolist.api.controller;
+
+import com.marcinwo.todolist.api.ApiInfo;
+import com.marcinwo.todolist.api.dto.LoggedInUserDTO;
+import com.marcinwo.todolist.api.dto.PatchUserDTO;
+import com.marcinwo.todolist.api.dto.UserDTO;
+import com.marcinwo.todolist.api.mapper.UserMapper;
+import com.marcinwo.todolist.app.security.annotation.IsAuthenticated;
+import com.marcinwo.todolist.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+    private UserMapper userMapper;
+
+    @Autowired
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @IsAuthenticated//ok
+    @GetMapping("/user")
+    public LoggedInUserDTO getLoggedInUser() {
+        return userMapper.toLoggedInUserDTO(userService.findCurrentUser());
+    }
+
+    @GetMapping("/users")//ok
+    public ResponseEntity<List<? extends UserDTO>> getUsers() {
+        if(userService.findCurrentUser().hasRole("ROLE_ADMIN")) {
+            return ResponseEntity.ok(userMapper.toLoggedInUserDTO(userService.findAll()));
+        } else {
+            return ResponseEntity.ok(userMapper.toUserDto(userService.findAll()));
+        }
+    }
+
+    @IsAuthenticated
+    @GetMapping("/users/{id}")//ok
+    public UserDTO getUser(@PathVariable Long id) {
+        if (userService.findCurrentUser().hasRole("ROLE_ADMIN")) {
+            return userMapper.toLoggedInUserDTO(userService.findById(id));
+        } else {
+            return  userMapper.toUserDto(userService.findById(id));
+        }
+    }
+
+    @PostMapping("/users")//ok
+    public UserDTO postUser(@RequestBody UserDTO userDTO) {
+        //email, sms service
+        return userMapper.toUserDto(userService.save(userMapper.toUserEntity(userDTO)));
+    }
+
+
+    @IsAuthenticated//ok
+    @PreAuthorize("hasRole('ROLE_ADMIN') or userService.findCurrentUser().getId().equals(#id)")
+    @PatchMapping("/users/{id}")
+    public LoggedInUserDTO updateUser(@PathVariable Long id, @RequestBody PatchUserDTO patchUserDTO) {
+            return userMapper.toLoggedInUserDTO(userService.updateUser(id, patchUserDTO));
+
+    }
+
+    @IsAuthenticated//ok
+    @PreAuthorize("hasRole('ROLE_ADMIN') or userService.findCurrentUser().getId().equals(#id)")
+    @DeleteMapping("/users/{id}") //ok
+    public ResponseEntity<ApiInfo> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(new ApiInfo("User deleted.", HttpStatus.OK.value()), HttpStatus.OK);
+    }
+
+}
