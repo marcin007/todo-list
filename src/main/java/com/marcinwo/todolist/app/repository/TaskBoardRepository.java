aee99b06fa1154b85841c20891fd6facfc7e6c66
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-14 23:11:04 +0200

    Added TaskBoardRepository init.

---
+++ src/main/java/com/marcinwo/todolist/app/repository/TaskBoardRepository.java
@@ -0,0 +1,15 @@
+package com.marcinwo.todolist.app.repository;
+
+import com.marcinwo.todolist.app.entity.TasksBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface TaskBoardRepository extends JpaRepository<TasksBoard, Long> {
+    Optional<TasksBoard> findByIdAndOwnerIdAndHasExpiredFalse(Long id, Long userId);
+    //Set<TasksBoard> findAllAndHasExpiredFalse();
+    Optional<TasksBoard> findAllByIdAndHasExpiredFalse(Long id);
+}
