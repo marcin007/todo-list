c19b35e17bfca0b9bfa62a116ba54fb61da0e9ef
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-14 23:11:40 +0200

    Added TaskRepository init.

---
+++ src/main/java/com/marcinwo/todolist/app/repository/TaskRepository.java
@@ -0,0 +1,20 @@
+package com.marcinwo.todolist.app.repository;
+
+import com.marcinwo.todolist.app.entity.Task;
+import com.marcinwo.todolist.app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long > {
+    Set<Task> findAllByUsersIdAndHasExpiredFalse(Long userId);
+    Optional<Task>findByIdAndHasExpiredFalse(Long ig);
+    Set<Task> findAllByUsersAndHasExpiredFalse(User user);
+
+    List<Task> findAllByUsersUserNameAndHasExpiredFalse(String username);
+
+}
