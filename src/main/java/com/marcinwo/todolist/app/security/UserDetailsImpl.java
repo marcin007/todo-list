d5302c614b467110486dba192e3f03f5f7531b82
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-14 23:11:54 +0200

    Added UserDetailsImpl init.

---
+++ src/main/java/com/marcinwo/todolist/app/security/UserDetailsImpl.java
@@ -0,0 +1,55 @@
+package com.marcinwo.todolist.app.security;
+
+import com.marcinwo.todolist.app.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !user.getIsExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !user.getIsLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !user.getIsCredentialsExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getIsEnabled();
+    }
+}
