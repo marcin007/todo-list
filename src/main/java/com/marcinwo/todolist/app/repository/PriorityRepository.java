592f60d7bada0b2402b56addd0c165fa1f50f23d
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-18 16:26:39 +0200

    Changed imports paths for PriorityRepository

--- src/main/java/com/marcinwo/todolist/app/repository/PriorityRepository.java
+++ src/main/java/com/marcinwo/todolist/app/repository/PriorityRepository.java
@@ -1,6 +1,6 @@
-package com.marcinwo.todolist.repository;
+package com.marcinwo.todolist.app.repository;
 
-import com.marcinwo.todolist.entity.Priority;
+import com.marcinwo.todolist.app.entity.Priority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
