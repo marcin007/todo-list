44bba205f951a5a92c706c93da8f0319653eb7db
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-20 12:56:31 +0200

    Added update, save, find and delete methods in TaskBoardService

--- src/main/java/com/marcinwo/todolist/app/service/TaskBoardService.java
+++ src/main/java/com/marcinwo/todolist/app/service/TaskBoardService.java
@@ -1,16 +1,15 @@
 package com.marcinwo.todolist.app.service;
 
 import com.marcinwo.todolist.api.dto.PatchTaskBoardDTO;
-import com.marcinwo.todolist.app.entity.Task;
+import com.marcinwo.todolist.api.dto.TasksBoardDTO;
 import com.marcinwo.todolist.app.entity.TasksBoard;
-import com.marcinwo.todolist.app.exception.TaskBoardNotFoundException;
-import com.marcinwo.todolist.app.exception.TaskNotFoundException;
+import com.marcinwo.todolist.app.exception.TasksBoardNotFoundException;
+import com.marcinwo.todolist.app.exception.UserUnauthorizedException;
 import com.marcinwo.todolist.app.repository.TaskBoardRepository;
 import com.marcinwo.todolist.app.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -27,33 +26,57 @@ public class TaskBoardService {
         this.taskRepository = taskRepository;
     }
 
-    public List<TasksBoard> findAll() {
-        return taskBoardRepository.findAll().stream()
-                .filter(tasksBoard -> !tasksBoard.isHasExpired())
-                .collect(Collectors.toList());
+    public Set<TasksBoard> findAll(String username) {
+        return taskBoardRepository.findAllByUsersUserName(username);
+    }
+    public List<TasksBoard> AdminFindAll() {
+        return taskBoardRepository.findAll();
     }
 
     public TasksBoard findById(Long id) {
-        return taskBoardRepository.findById(id)
-                .filter(t -> !t.isHasExpired())
-                .orElseThrow(() -> new TaskBoardNotFoundException("Tasks Board not found."));
+        return taskBoardRepository.findAllByIdAndHasExpiredFalse(id)
+                .orElseThrow(() -> new TasksBoardNotFoundException("Tasks Board not found."));
     }
 
     public TasksBoard updateTasksBoard(Long id, PatchTaskBoardDTO patchTaskBoardDTO) {
         TasksBoard tasksBoard = findById(id);
-        if(patchTaskBoardDTO.getColour() != null){
+        return updateTaskBoard(tasksBoard, patchTaskBoardDTO);
+    }
+
+    public TasksBoard updateTaskBoard(TasksBoard tasksBoard, PatchTaskBoardDTO patchTaskBoardDTO) {
+        if (patchTaskBoardDTO.getColour() != null) {
             tasksBoard.setColour(patchTaskBoardDTO.getColour());
         }
-        if(patchTaskBoardDTO.getName() != null){
+        if (patchTaskBoardDTO.getName() != null) {
             tasksBoard.setName(patchTaskBoardDTO.getName());
         }
         return taskBoardRepository.save(tasksBoard);
     }
 
+    public TasksBoard updateUserTasksBoard(Long id, Long currentUserId, PatchTaskBoardDTO patchTaskBoardDTO) {
+        TasksBoard tasksBoard = findById(id);
+        if (!tasksBoard.getOwner().getId().equals(currentUserId)) {
+            throw new UserUnauthorizedException("User unauthorized.");
+        }
+
+        return updateTaskBoard(tasksBoard, patchTaskBoardDTO);
+    }
+
     public void deleteById(Long id) {
-    TasksBoard tasksBoard = findById(id);
-    tasksBoard.setHasExpired(true);
-    taskBoardRepository.save(tasksBoard);
+        TasksBoard tasksBoard = findById(id);
+        tasksBoard.setHasExpired(true);
+        taskBoardRepository.save(tasksBoard);
+    }
+
+    public void deleteUserTasksBoardById(Long id, Long userId) {
+        TasksBoard tasksBoard = taskBoardRepository.findByIdAndOwnerIdAndHasExpiredFalse(id, userId)
+                .orElseThrow(() -> new TasksBoardNotFoundException("Tasks Board not found."));
+        tasksBoard.setHasExpired(true);
+        taskBoardRepository.save(tasksBoard);
+    }
+
+    public TasksBoard save(TasksBoard tasksBoard) {
+        return taskBoardRepository.save(tasksBoard);
     }
 }
 
