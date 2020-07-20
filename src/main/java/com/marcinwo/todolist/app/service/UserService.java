45c4b6fe193913aa6752e0f02f72246df0be0433
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-20 12:58:50 +0200

    Added find, save, update and delete methods in UserService

--- src/main/java/com/marcinwo/todolist/app/service/UserService.java
+++ src/main/java/com/marcinwo/todolist/app/service/UserService.java
@@ -11,43 +11,52 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class UserService {
 
     private UserRepository userRepository;
     private ActivationCodeHandler activationCodeHandler;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository, ActivationCodeHandler activationCodeHandler) {
+    public UserService(UserRepository userRepository, ActivationCodeHandler activationCodeHandler, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.activationCodeHandler = activationCodeHandler;
+        this.passwordEncoder = passwordEncoder;
     }
-    @Autowired
-    PasswordEncoder passwordEncoder;
 
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
+    public List<User> findAllWithoutCurrentUser() {
+        List<User> users = userRepository.findAll();
+        users.remove(findCurrentUser());
+        return users;
+    }
+
     public User findCurrentUser(){
-        return userRepository.findByUserName(SecurityUtils.getUsername())
+        return userRepository.findByUserNameAndHasExpiredFalse(SecurityUtils.getUsername())
                 .orElseThrow(()->new UserNotFoundException("Username not found."));
     }
 
     public User findById(Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found."));
+        return userRepository.findByIdAndHasExpiredFalse(id).orElseThrow(() -> new UserNotFoundException("User not found."));
     }
 
     public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setActivationCode(activationCodeHandler.generateActivationCode());
         return userRepository.save(user);
     }
 
     public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+        User user = userRepository.findByIdAndHasExpiredFalse(id).orElseThrow(() -> new UserNotFoundException("User not found."));
+        user.setIsExpired(true);
+        userRepository.save(user);
     }
-
 
 
     public User updateUser(Long id, PatchUserDTO patchUserDTO) {
@@ -68,5 +77,12 @@ public class UserService {
             user.setPassword(passwordEncoder.encode(patchUserDTO.getPassword()));
         }
         return userRepository.save(user);
+    }
+
+
+
+
+    public User findByUsername(String username) {
+        return userRepository.findByUserNameAndHasExpiredFalse(username).orElseThrow(()-> new RuntimeException("sd"));
     }
 }
