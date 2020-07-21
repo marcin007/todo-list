c75b66242f35d63655cbee96439bf0dd954d8f18
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-21 11:55:27 +0200

    Added getters, setters in TaskDTO

--- src/main/java/com/marcinwo/todolist/api/dto/TaskDTO.java
+++ src/main/java/com/marcinwo/todolist/api/dto/TaskDTO.java
@@ -1,15 +1,24 @@
 package com.marcinwo.todolist.api.dto;
 
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskDTO {
 
     private Long taskBoardId;
     private Long priorityId;
-    private Long userId;
+    private Long owner;
     private String name;
     private String description;
     private String status;
     private LocalDateTime deadline;
     private LocalDateTime reminder;
+    private Boolean hasExpired = false;
 }
