9c339d38899d35462d87d8063686d4278d4f042a
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-23 21:43:22 +0200

    Added JwtAuthenticationResponse init.

---
+++ src/main/java/com/marcinwo/todolist/app/security/jwt/JwtAuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.marcinwo.todolist.app.security.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtAuthenticationResponse {
+    private String username;
+    private String token;
+}
