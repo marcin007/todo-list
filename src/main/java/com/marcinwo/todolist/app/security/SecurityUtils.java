ac36dd6b4092e1625d404303351c884641d8ccdc
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-24 10:32:08 +0200

    Added getUsername, isUserLoggedIn in SecurityUtils init.

--- src/main/java/com/marcinwo/todolist/app/security/SecurityUtils.java
+++ src/main/java/com/marcinwo/todolist/app/security/SecurityUtils.java
@@ -2,6 +2,11 @@ package com.marcinwo.todolist.app.security;
 
 import com.vaadin.flow.server.ServletHelper;
 import com.vaadin.flow.shared.ApplicationConstants;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.stream.Stream;
@@ -15,4 +20,24 @@ public class SecurityUtils {
         return parameterValue != null
                 && Stream.of(ServletHelper.RequestType.values()).anyMatch(r -> r.getIdentifier().equals(parameterValue));
     }
+
+    public static String getUsername() {
+        SecurityContext context = SecurityContextHolder.getContext(); //
+        Object principal = context.getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) principal;
+            return userDetails.getUsername();
+        }
+
+        return null;
+    }
+
+    public static boolean isUserLoggedIn() {
+        return isUserLoggedIn(SecurityContextHolder.getContext().getAuthentication());
+    }
+
+    public static boolean isUserLoggedIn(Authentication authentication) {
+        return authentication != null && !(authentication instanceof AnonymousAuthenticationToken);
+    }
+
 }
