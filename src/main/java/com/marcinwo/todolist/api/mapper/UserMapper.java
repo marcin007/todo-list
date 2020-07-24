c2b85811ff183c85eddcc3516d5da74b7b6838f0
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-24 10:38:59 +0200

    Added toUserDto, to LoggedInUserDTO and toUserEntity in UserMapper

--- src/main/java/com/marcinwo/todolist/api/mapper/UserMapper.java
+++ src/main/java/com/marcinwo/todolist/api/mapper/UserMapper.java
@@ -4,20 +4,69 @@ import com.marcinwo.todolist.api.dto.LoggedInUserDTO;
 import com.marcinwo.todolist.api.dto.UserDTO;
 import com.marcinwo.todolist.app.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper {
+public abstract class UserMapper {
 
-    UserDTO toUserDto(User user);
-    List<UserDTO> toUserDto(Collection<User> userCollection);
+    public UserDTO toUserDto(User user) {
+        if (user == null) {
+            return null;
+        }
 
+        UserDTO userDTO = new UserDTO();
 
-    LoggedInUserDTO toLoggedInUserDTO(User user);
+        userDTO.setId(user.getId());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setUserName(user.getUserName());
+        userDTO.setHasExpired(user.getHasExpired());
 
-    User toUserEntity(UserDTO userDTO);
+        return userDTO;
+    }
+
+    public List<UserDTO> toUserDto(Collection<User> users) {
+        return users.stream().map(this::toUserDto).collect(Collectors.toList());
+    }
+
+    public LoggedInUserDTO toLoggedInUserDTO(User user) {
+        if (user == null) {
+            return null;
+        }
+
+        LoggedInUserDTO loggedInUserDTO = new LoggedInUserDTO();
+
+        loggedInUserDTO.setId(user.getId());
+        loggedInUserDTO.setFirstName(user.getFirstName());
+        loggedInUserDTO.setLastName(user.getLastName());
+        loggedInUserDTO.setUserName(user.getUserName());
+        loggedInUserDTO.setHasExpired(user.getHasExpired());
+        loggedInUserDTO.setAvatarUrl(user.getAvatarUrl());
+        loggedInUserDTO.setEmail(user.getEmail());
+
+        return loggedInUserDTO;
+    }
+
+    public List<LoggedInUserDTO> toLoggedInUserDTO(Collection<User> users) {
+        return users.stream().map(this::toLoggedInUserDTO).collect(Collectors.toList());
+    }
+
+    public User toUserEntity(UserDTO userDTO) {
+        if (userDTO == null) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setId(userDTO.getId());
+        user.setHasExpired(userDTO.getHasExpired());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setUserName(userDTO.getUserName());
+
+        return user;
+    }
 }
