83d98f35dec36a5f5f6c5c3f72763b3ce21e1874
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-25 13:52:51 +0200

    Added owner, collaborators and hasExpired field to TaskBoardDTO

--- src/main/java/com/marcinwo/todolist/api/dto/TasksBoardDTO.java
+++ src/main/java/com/marcinwo/todolist/api/dto/TasksBoardDTO.java
@@ -1,10 +1,23 @@
 package com.marcinwo.todolist.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TasksBoardDTO {
 
     private Long id;
-    private Long userId;
+    private String owner;
     private String name;
     private String colour;
+    private Set<String> collaborators;
+    private Boolean hasExpired = false;
 
 }
