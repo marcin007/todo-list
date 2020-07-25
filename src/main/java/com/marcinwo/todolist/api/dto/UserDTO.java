f7a2e24dcd1a019b9e8b93f8e1ad512215203dcb
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-25 13:48:23 +0200

    Added getters and setters to UserDTO

--- src/main/java/com/marcinwo/todolist/api/dto/UserDTO.java
+++ src/main/java/com/marcinwo/todolist/api/dto/UserDTO.java
@@ -1,9 +1,19 @@
 package com.marcinwo.todolist.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
 
     private Long id;
     private String firstName;
     private String lastName;
     private String userName;
+    private Boolean hasExpired = false;
 }
