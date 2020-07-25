3571b0634958e52307b7736f0da0c99a1bf83a89
marcin007 <marcin007wojciechowski@gmail.com>
2020-07-25 13:50:13 +0200

    Changed path for TaskService and added methods findAllByUsername, save and updateTask

--- src/main/java/com/marcinwo/todolist/app/service/TaskService.java
+++ src/main/java/com/marcinwo/todolist/app/service/TaskService.java
@@ -1,11 +1,15 @@
 package com.marcinwo.todolist.app.service;
 
+import com.marcinwo.todolist.api.dto.PatchTaskDTO;
 import com.marcinwo.todolist.app.entity.Task;
+import com.marcinwo.todolist.app.exception.TaskNotFoundException;
 import com.marcinwo.todolist.app.exception.UserNotFoundException;
 import com.marcinwo.todolist.app.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -19,17 +23,49 @@ public class TaskService {
     }
 
     public Task findById(Long id) {
-        return taskRepository.findByIdAndHasExpiredFalse(id).orElseThrow(()-> new UserNotFoundException("user not found."));
+        return taskRepository.findByIdAndHasExpiredFalse(id).orElseThrow(() -> new TaskNotFoundException("Task not found."));
     }
 
     public void deleteById(Long id) {
-    Task task = findById(id);
-    task.setHasExpired(true);
-    taskRepository.save(task);
+        Task task = findById(id);
+        task.setHasExpired(true);
+        taskRepository.save(task);
+    }
+
+    public Set<Task> findAllByUserId(Long id) {
+        return taskRepository.findAllByUsersIdAndHasExpiredFalse(id);
     }
 
 
-    public Set<Task> findAllByUserId(Long id) {
-        return taskRepository.findAllByUsersId(id);
+    public List<Task> findAllByUsername(String username) {
+        return taskRepository.findAllByUsersUserNameAndHasExpiredFalse(username);
+    }
+
+    public Task save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, PatchTaskDTO patchTaskDTO) {
+        Task task = findById(id);
+        if (patchTaskDTO.getName() != null) {
+            task.setName(patchTaskDTO.getName());
+        }
+        if (patchTaskDTO.getDescription() != null) {
+            task.setDescription(patchTaskDTO.getDescription());
+        }
+        if (patchTaskDTO.getStatus() != null) {
+            task.setStatus(patchTaskDTO.getStatus());
+        }
+        if (patchTaskDTO.getDeadline() != null) {
+            task.setDeadline(patchTaskDTO.getDeadline());
+        }
+        if(patchTaskDTO.getReminder() != null){
+            task.setReminder(patchTaskDTO.getReminder());
+        }
+      return taskRepository.save(task);
+    }
+
+    public Task updateTask(String username, Long id) {
+    return null;
     }
 }
