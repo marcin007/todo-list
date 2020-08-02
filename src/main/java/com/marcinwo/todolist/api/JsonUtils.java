ad5442114235e05cae61aa9f9dd545ac62c80a23
marcin007 <marcin007wojciechowski@gmail.com>
2020-08-03 00:34:34 +0200

    Added JsonUtils init

---
+++ src/main/java/com/marcinwo/todolist/api/JsonUtils.java
@@ -0,0 +1,18 @@
+package com.marcinwo.todolist.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JsonUtils {
+    private JsonUtils() {}
+
+    public static String toJsonString(Object object) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return objectMapper.writeValueAsString(object);
+    }
+}
