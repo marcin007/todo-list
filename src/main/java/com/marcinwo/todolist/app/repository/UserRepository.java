000392464507212886c9e5f6b2559ec0400f07e3
marcin007 <marcin007wojciechowski@gmail.com>
2020-08-09 23:11:25 +0200

    Added new methods to UserRepository

--- src/main/java/com/marcinwo/todolist/app/repository/UserRepository.java
+++ src/main/java/com/marcinwo/todolist/app/repository/UserRepository.java
@@ -4,11 +4,17 @@ import com.marcinwo.todolist.app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserName(String username);
+
     Optional<User> findByUserNameAndHasExpiredFalse(String username);
+    boolean existsByUserNameAndHasExpiredFalse(String username);
     Optional<User> findByIdAndHasExpiredFalse(Long id);
+    Optional<User> findByUserNameAndIsEnabledTrueAndHasExpiredFalse(String username);
+    List<User> findAllByHasExpiredFalse();
+
 }
